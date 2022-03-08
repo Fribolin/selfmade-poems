e5ee47961e66933439b0ace538dd780d50e2a2d2
emilieiios <89915196+emilieiios@users.noreply.github.com>
2022-03-08 14:10:15 +0100

    Favorites hinzugefügt

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from blog.models import Post
 from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     image = models.ImageField(default = "default.jpg", upload_to = "profile_pics")
+    favorites = models.ManyToManyField(Post)
 
     def __str__(self):
         return f'{self.user.username} Profile'
