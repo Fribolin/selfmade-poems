5240fcce4876602f7f740c988ee44053e85e7738
KuroKurama01 <yannicschalk02@gmail.com>
2022-03-15 10:29:22 +0100

    Added CommentView

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -9,25 +9,27 @@ class Category(models.TextChoices):
     NATURE = 'NA', ('Nature')
     LIFE = 'LI', ('Life')
 
-class Comment(models.Model):
-    name = models.CharField(max_length=80, default="")
-    body = models.TextField()
-    date_posted = models.DateTimeField(default = timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
     date_posted = models.DateTimeField(default = timezone.now)
     category = models.CharField(max_length = 30, choices=Category.choices, default="")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    comments = models.ManyToManyField(Comment)
+    #comments = models.ManyToManyField(Comment)
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=80, default="")
+    body = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments", default="")
+    
 
 
